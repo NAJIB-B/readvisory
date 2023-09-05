71c5de440bef6ead664992fdbeb6fad834686429
Muhammad Najib Bala <najibajibasu@gmail.com>
2023-09-05 10:24:16 +0100

    Added error and rsucess messages for form submissions.

--- src/components/propertyEnquiry/whiteForm.jsx
+++ src/components/propertyEnquiry/whiteForm.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
+import { Toaster, toast } from 'react-hot-toast';
 
 import { InputField } from '../custormFormField';
 import { Text } from '../Text';
@@ -13,10 +14,18 @@ export const WhiteForm = () => {
   const {
     register,
     handleSubmit,
+    resetField,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phoneNumber: '',
+      message: '',
+      subject: '',
+    },
+  });
   const SubmitHandler = ({ name, email, phoneNumber, message, subject }) => {
-    console.log(name, message, subject, email, phoneNumber);
     emailjs
       .send(
         serviceId,
@@ -30,132 +39,142 @@ export const WhiteForm = () => {
         },
         publicKey
       )
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+      .then(() => {
+        toast.success('Successfully sent');
+        resetField('email');
+        resetField('message');
+        resetField('name');
+        resetField('phoneNumber');
+        resetField('subject');
+      })
+      .catch(() => toast.error('Something went wrong. Try again'));
   };
 
   return (
-    <form
-      className="py-12 lg:px-10 md:px-10 sm:px-4 xs:px-4 xxs:px-4 xl:px-10"
-      onSubmit={handleSubmit(SubmitHandler)}
-    >
-      <div className="grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mb-5">
-        {[
-          {
-            title: 'name',
-            labelName: 'Name',
-            selectArrayOption: null,
-            type: 'text',
-            error: errors.name,
-            placeHold: 'john doe',
-            required: true,
-          },
-          {
-            title: 'email',
-            labelName: 'Email',
-            selectArrayOption: null,
-            type: 'email',
-            error: errors.email,
-            placeHold: '',
-            required: true,
-          },
-          {
-            title: 'phoneNumber',
-            labelName: 'Phone Number',
-            selectArrayOption: null,
-            type: 'number',
-            error: errors.phoneNumber,
-            placeHold: '+234 676 898 787',
-            required: false,
-          },
-        ].map((option, index) => {
-          const {
-            title,
-            labelName,
-            placeHold,
-            selectArrayOption,
-            type,
-            error,
-            required,
-          } = option;
-          switch (type) {
-            case 'select':
-              return (
-                <InputField
-                  key={index}
-                  name={title}
-                  type={type}
-                  placeHolder={placeHold}
-                  labelTitle={labelName}
-                  labelStyle="text-sm font-bold text-start mb-1"
-                  selectArrayOption={selectArrayOption}
-                  register={register}
-                  errors={error}
-                  style="w-full text-start p-3 text-xs border-bottom"
-                />
-              );
-              break;
-            default:
-              return (
-                <InputField
-                  key={index}
-                  name={title}
-                  placeHolder={placeHold}
-                  type={type}
-                  labelTitle={labelName}
-                  labelStyle="text-sm font-bold text-start mb-1"
-                  register={register}
-                  errors={error}
-                  required={required}
-                  style="w-full text-start p-3 text-xs border-bottom"
-                />
-              );
-          }
-        })}
-      </div>
-      <div>
-        <Text style="font-semibold text-sm mb-4" value="Select Subject?" />
-        <div className="flex mb-5 flex-wrap">
-          {['Advisory', 'Development', 'Management', 'General EnQuiry'].map(
-            (rad, index) => {
-              return (
-                <InputField
-                  key={index}
-                  name="subject"
-                  value={rad}
-                  type="radio"
-                  labelTitle={rad}
-                  labelStyle="text-xs font-bold text-start text-black mb-1 me-8"
-                  register={register}
-                  required={true}
-                  errors={errors.subject}
-                  style="text-start p-3 text-xs me-2"
-                />
-              );
+    <>
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <form
+        className="py-12 lg:px-10 md:px-10 sm:px-4 xs:px-4 xxs:px-4 xl:px-10"
+        onSubmit={handleSubmit(SubmitHandler)}
+      >
+        <div className="grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 mb-5">
+          {[
+            {
+              title: 'name',
+              labelName: 'Name',
+              selectArrayOption: null,
+              type: 'text',
+              error: errors.name,
+              placeHold: 'john doe',
+              required: true,
+            },
+            {
+              title: 'email',
+              labelName: 'Email',
+              selectArrayOption: null,
+              type: 'email',
+              error: errors.email,
+              placeHold: '',
+              required: true,
+            },
+            {
+              title: 'phoneNumber',
+              labelName: 'Phone Number',
+              selectArrayOption: null,
+              type: 'number',
+              error: errors.phoneNumber,
+              placeHold: '+234 676 898 787',
+              required: false,
+            },
+          ].map((option, index) => {
+            const {
+              title,
+              labelName,
+              placeHold,
+              selectArrayOption,
+              type,
+              error,
+              required,
+            } = option;
+            switch (type) {
+              case 'select':
+                return (
+                  <InputField
+                    key={index}
+                    name={title}
+                    type={type}
+                    placeHolder={placeHold}
+                    labelTitle={labelName}
+                    labelStyle="text-sm font-bold text-start mb-1"
+                    selectArrayOption={selectArrayOption}
+                    register={register}
+                    errors={error}
+                    style="w-full text-start p-3 text-xs border-bottom"
+                  />
+                );
+                break;
+              default:
+                return (
+                  <InputField
+                    key={index}
+                    name={title}
+                    placeHolder={placeHold}
+                    type={type}
+                    labelTitle={labelName}
+                    labelStyle="text-sm font-bold text-start mb-1"
+                    register={register}
+                    errors={error}
+                    required={required}
+                    style="w-full text-start p-3 text-xs border-bottom"
+                  />
+                );
             }
-          )}
+          })}
         </div>
-        <div className="mb-5">
-          <InputField
-            name="message"
-            type="text"
-            placeHolder="Write your message"
-            labelTitle="Message"
-            labelStyle="text-sm font-bold text-start text-black mb-1"
-            register={register}
-            errors={errors.message}
-            required={true}
-            style="text-start p-3 text-xs border-bottom me-4"
+        <div>
+          <Text style="font-semibold text-sm mb-4" value="Select Subject?" />
+          <div className="flex mb-5 flex-wrap">
+            {['Advisory', 'Development', 'Management', 'General EnQuiry'].map(
+              (rad, index) => {
+                return (
+                  <InputField
+                    key={index}
+                    name="subject"
+                    value={rad}
+                    type="radio"
+                    labelTitle={rad}
+                    labelStyle="text-xs font-bold text-start text-black mb-1 me-8"
+                    register={register}
+                    required={true}
+                    errors={errors.subject}
+                    style="text-start p-3 text-xs me-2"
+                  />
+                );
+              }
+            )}
+          </div>
+          <div className="mb-5">
+            <InputField
+              name="message"
+              type="text"
+              placeHolder="Write your message"
+              labelTitle="Message"
+              labelStyle="text-sm font-bold text-start text-black mb-1"
+              register={register}
+              errors={errors.message}
+              required={true}
+              style="text-start p-3 text-xs border-bottom me-4"
+            />
+          </div>
+        </div>
+        <div>
+          <Button
+            buttonStyle="px-12 py-2 m-auto bg-dark-moderate"
+            textStyle="text-white"
+            text="submit"
           />
         </div>
-      </div>
-      <div>
-        <Button
-          buttonStyle="px-12 py-2 m-auto bg-dark-moderate"
-          textStyle="text-white"
-          text="submit"
-        />
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
