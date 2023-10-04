e1b7bae7669cadd334d0970dfcbc9ce8859ef56a
Muhammad Najib Bala <najibajibasu@gmail.com>
2023-10-04 09:57:39 +0100

    edited page title

--- src/app/advisory/page.jsx
+++ src/app/advisory/page.jsx
@@ -5,7 +5,7 @@ import Bgimage from '../../../public/images/and.png';
 import { Acquisition } from '@/components/advisoryComponent/acquisition';
 
 export const metadata = {
-  title: 'Property advise, asset transfer property management | Advisory',
+  title: 'Property advise, asset transfer, property management | Advisory',
   description:
     'Independent property firm, offering real estate investment, finance, development and management services',
   robots: 'all',
